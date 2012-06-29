601b68c1d128dbc260109710a5a2aa5c295c73ba
Yann Klis <yann.klis@novelys.com>
2012-06-29 21:49:13 +0200

    simplify poster method

--- lib/imdb/movie.rb
+++ lib/imdb/movie.rb
@@ -82,12 +82,6 @@ module Imdb
     # Returns a string containing the URL to the movie poster.
     def poster
       src = document.at("#img_primary img")['src'] rescue nil
-      case src
-      when /^(http:.+@@)/
-        $1 + '.jpg'
-      when /^(http:.+?)\.[^\/]+$/
-        $1 + '.jpg'
-      end
     end
 
     # Returns a float containing the average user rating
